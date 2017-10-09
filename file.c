123fdfa99c66ea28d6e7d75ea6292b533fe423c2
Chris Stahly <cstahly@arxan.com>
2017-10-09 18:54:31 -0400

    initial commit

---
+++ file.c
@@ -0,0 +1,18 @@
+#include "stdio.h" // include standard input/output functions
+
+void print_some_butts(void) {
+	printf("butts ");
+	//printf("you are my one dad i love you love Bits ");
+	return;
+}
+
+void main(void) {
+	int x = 0;
+	printf("chicken and... ");
+	while (x<3) {
+		print_some_butts();
+		x++;
+	}
+	printf("\n");
+	return;
+}
